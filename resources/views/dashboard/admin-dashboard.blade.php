9db7379f219436c2ee589965c6c215d28957bfa7
vagrant <vagrant@homestead>
2021-07-15 16:32:31 +0000

    few things

--- resources/views/dashboard/admin-dashboard.blade.php
+++ resources/views/dashboard/admin-dashboard.blade.php
@@ -1,6 +1,8 @@
 
 <x-layout>
 
+
+
     <link rel="stylesheet" href="style.css">
         <section>
             <div class="container">
@@ -31,8 +33,8 @@
                                     <td>{{$user['username']}}</td>
                                     <td>{{$user['email']}}</td>
                                     <td>
-                                        <a href="admin/delete/{{$user['id']}}">Delete</a> |
-                                        <a href="admin/edit/{{$user['id']}}">Edit</a>
+                                        <a href="admin/delete/{{$user['id']}}">Delete</a>
+                                        <a href="admin/edit/{{$user['id']}}">edit</a>
                                     </td>
                                 </tr>
                             @endforeach
@@ -40,7 +42,67 @@
                     @endif
 
                     @if(isset($data))
-                        <x-AdminEditForm/>
+                        <form>
+                            <div class="mb-6">
+                                <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
+                                    username :
+                                </label>
+                                <input class="border border-gray-400 p-2 w-full"
+                                       type="text"
+                                       name="username"
+                                       id="username"
+                                       value = {{$data['username']}}
+                                >
+
+                            </div>
+
+
+                            <div class="mb-6">
+                                <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
+                                    email :
+                                </label>
+
+                                <input class="border border-gray-400 p-2 w-full"
+                                       type="email"
+                                       name="email"
+                                       id="email"
+                                       value = {{$data['email']}}
+                                >
+
+                            </div>
+
+
+                            <div class="mb-6">
+                                <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
+                                   New password :
+                                </label>
+                                <input class="border border-gray-400 p-2 w-full"
+                                       type="password"
+                                       name="password"
+                                       id="password"
+
+                                >
+
+                            </div>
+
+                            <div class="mb-6">
+                                <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="role">
+                                    Role :
+                                </label>
+                                <select name="role" id="role" class="border border-gray-400 p-2 w-full">
+                                    <option value="user">User</option>
+                                    <option value="admin">Admin</option>
+                                </select>
+
+                            </div>
+
+                            <div class="mb-6">
+                                <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                                    Submit
+                                </button>
+                            </div>
+
+                        </form>
                     @endif
 
 
